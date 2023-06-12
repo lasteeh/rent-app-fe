f75f720e7964324209102d18f8b191213f9cf839
Last David Lantican <lastdavid.lantican@gmail.com>
2023-06-12 10:19:58 +0800

    change base config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rent-app-fe/dist/",
+  base: "/rent-app-fe/",
   plugins: [react()],
 });
